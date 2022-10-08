ac980920729f939110e6a16b007b043ebdf0d337
Yash Parwani <parwaniyash5610@gmail.com>
2022-10-08 13:10:35 +0530

    updating api 2

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ import ProjectsCard from "../components/projectsCard";
 export async function getServerSideProps(context) {
   // fetches data about me
    // fetch returns a promise consisting of data in form of string so we need to convert it to JSON using .json()
-   const response = await fetch("https://yash-parwani.vercel.app/api/aboutme");
+   const response = await fetch("https://yash-parwani.netlify.app/api/aboutme");
    //.json() also returns a promise ,hence async await
    const datajson = await response.json();
 
