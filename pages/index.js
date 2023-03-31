791daf57eb1b430e31cba4651bd8cfa27ccf7ab4
Yash Parwani <95875897+Yash-Parwani@users.noreply.github.com>
2023-03-31 09:49:12 +0530

    Update index.js
    updating urls

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,11 @@ import ProjectsCard from "../components/projectsCard";
 export async function getServerSideProps(context) {
   // fetches data about me
    // fetch returns a promise consisting of data in form of string so we need to convert it to JSON using .json()
-   const response = await fetch("https://yash-parwani.herokuapp.com/api/aboutme");
+   const response = await fetch("https://yash-parwani-portfolio-production.vercel.app/api/aboutme");
    //.json() also returns a promise ,hence async await
    const datajson = await response.json();
 
-   const responsenew = await fetch("https://yash-parwani.netlify.app/api/projects");
+   const responsenew = await fetch("https://yash-parwani-portfolio-production.vercel.app/api/projects");
    const projects = await responsenew.json();
   return {
     props: {data: datajson,
